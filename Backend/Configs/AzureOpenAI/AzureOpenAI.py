ecaa44e693c114d592a8556668fe5456b006bc1e
UsmanGhani <usgh@matas.dk>
2025-11-24 00:07:31 +0100

    base config added

---
+++ Backend/Configs/AzureOpenAI/AzureOpenAI.py
@@ -0,0 +1,28 @@
+from ..BaseConfig import BaseConfig
+
+class AzureOpenAIConfig(BaseConfig):
+    def __init__(self, openai_api_key, azure_endpoint, deployment_name, api_version, temperature=0.7):
+        self.api_type = "azure"
+        self.openai_api_key = openai_api_key
+        self.azure_endpoint = azure_endpoint
+        self.deployment_name = deployment_name
+        self.api_version = api_version
+        self.temperature = temperature
+        super().__init__()
+    
+    def validate(self) -> None:
+        """Validate Azure OpenAI configuration."""
+        print("Validating Azure OpenAI configuration...")
+        
+        if not self.openai_api_key:
+            raise ValueError("API key is required")
+        if not self.azure_endpoint:
+            raise ValueError("Endpoint is required")
+        if not self.deployment_name:
+            raise ValueError("Deployment is required")
+        if not self.api_version:
+            raise ValueError("API Version is required")
+        if not self.temperature:
+            raise ValueError("Temperature is required")
+        
+        print("Azure OpenAI configuration is valid.")
